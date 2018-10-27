5f50159643598e2c1d282102cdb98242953fcf1a
GmGameFilm <gmgamefilm@gmail.com>
2018-10-27 12:54:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1],
-array2 = [1, 7,3],
-array3 = [1,undefined,3,5,-3],
-array4 = [1,NaN,3,5,-3];
-
-function min(array) {
-var currentElement;
-for (var i = 0; i < array.length; i++) {
-  if (isFinite(array[i])){
-    if(currentElement === undefined) currentElement = array[i];
-    if (array[i]<currentElement) currentElement = array[i];
-  }
+var array1 = [5, 3, 6, 4, 8, 2];
+function findInArray(array){
+	var posMax = array[0], posMin = array[0], sum = 0, extra, obj = {};
+	for(var i = 0; i<array.length; i++)	{
+		sum += array[i];
+		obj[i] = array[i];
+		if (array[posMax]<array[i+1]) posMax = i+1;
+		if (array[posMin]>array[i+1]) posMin = i+1;
+	}
+	obj['min'] = array[posMin];
+	obj['max'] = array[posMax];
+	obj['sum'] = sum;
+	extra = obj['min'];
+	obj[posMin] = obj['max'];
+	obj[posMax] = extra;		
+	return obj;
 }
-return currentElement;
-}
+console.log(findInArray(array1));
 
-function max(array) {
-  var currentElement;
-  for (var i = 0; i < array.length; i++) {
-    if (isFinite(array[i])){
-      if(currentElement === undefined) currentElement = array[i];
-      if (array[i]>currentElement) currentElement = array[i];
-    }
-  }
-  return currentElement;
+function cb(cbs){
+	return function(cbs){
+		cbs();
+	}
 }
 
-function sum(array) {
-  var count = 0;
-  for (var i = 0; i < array.length; i++) {
-    if (isFinite(array[i])) count += array[i];
-  }
-  return count;
+var array2 = ['s', 255, 34, 'a', 5];
+function arrToObj(array){
+	var obj = {};
+	for(var a = 0; a<array.length; a++){
+		obj[array[a]] = undefined;
+	}
+	return obj;
 }
-
-console.log(sum(array1)+" sum(array1)");
-console.log(sum(array2)+" sum(array2)");
-console.log(sum(array3)+" sum(array3)");
-console.log(sum(array4)+" sum(array4)");
-console.log("");
-console.log(min(array1)+" min(array1)");
-console.log(min(array2)+" min(array2)");
-console.log(min(array3)+" min(array3)");
-console.log(min(array4)+" min(array4)");
-console.log("");
-console.log(max(array1)+" max(array1)");
-console.log(max(array2)+" max(array2)");
-console.log(max(array3)+" max(array3)");
-console.log(max(array4)+" max(array4)");
+console.log(arrToObj(array2));
