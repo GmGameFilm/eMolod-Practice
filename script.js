27dc13607b12770cc078d6f6b0a4b370265e7136
GmGameFilm <gmgamefilm@gmail.com>
2018-11-10 15:26:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1],
-array2 = [1, 7,3],
-array3 = [1,undefined,3,5,-3],
-array4 = [1,NaN,3,5,-3];
-
-function min(array) {
-var currentElement;
-for (var i = 0; i < array.length; i++) {
-  if (isFinite(array[i])){
-    if(currentElement === undefined) currentElement = array[i];
-    if (array[i]<currentElement) currentElement = array[i];
-  }
-}
-return currentElement;
-}
-
-function max(array) {
-  var currentElement;
-  for (var i = 0; i < array.length; i++) {
-    if (isFinite(array[i])){
-      if(currentElement === undefined) currentElement = array[i];
-      if (array[i]>currentElement) currentElement = array[i];
+Array.prototype.myForEach = function (item, callback) {
+    for(i = item; i<this.length; i++){
+    	callback();
     }
-  }
-  return currentElement;
-}
+};
 
-function sum(array) {
-  var count = 0;
-  for (var i = 0; i < array.length; i++) {
-    if (isFinite(array[i])) count += array[i];
-  }
-  return count;
-}
+Array.prototype.myMap = function(callback) {
+	let tempArr = this;
+	let newArr;
+	for (i = item; i<this.length; i++) {
+		tempArr[i] = callback(); 
+		newArr.push(tempArr[i]);
+	}
+	return newArr;
+};
 
-console.log(sum(array1)+" sum(array1)");
-console.log(sum(array2)+" sum(array2)");
-console.log(sum(array3)+" sum(array3)");
-console.log(sum(array4)+" sum(array4)");
-console.log("");
-console.log(min(array1)+" min(array1)");
-console.log(min(array2)+" min(array2)");
-console.log(min(array3)+" min(array3)");
-console.log(min(array4)+" min(array4)");
-console.log("");
-console.log(max(array1)+" max(array1)");
-console.log(max(array2)+" max(array2)");
-console.log(max(array3)+" max(array3)");
-console.log(max(array4)+" max(array4)");
+Array.prototype.mySort = function(callback){
+	if(callback === undefined){
+		for(i = 0; i < this.length, i++){
+			if(this[i].toString()>this[i+]) {
+				let temp = this[i];
+				this[i] = this[i+1];
+				this[i+1] = temp;
+			}
+		}
+	} else {
+		callback();
+	}
+} 
